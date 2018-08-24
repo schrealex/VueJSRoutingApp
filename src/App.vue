8b9c2a021132afe89b873f81750797c1aba2c142
schrealex <cer34ll@gmail.com>
2018-08-24 10:25:48 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <page1/>
+  </div>
+</template>
+
+<script>
+import Page1 from "./components/Page1";
+
+export default {
+  name: "App",
+  components: {
+    Page1
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
